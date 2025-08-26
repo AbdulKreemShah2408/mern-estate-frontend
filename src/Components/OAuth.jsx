b71556d621d20eb7761ec775c447e35f5f702502
AbdulKreemShah2408 <abdulkreemshah92@gmail.com>
2025-08-26 11:29:00 -0700

    add all file update again

--- src/Components/OAuth.jsx
+++ src/Components/OAuth.jsx
@@ -1,35 +1,94 @@
-import React from 'react'
-import {getAuth, GoogleAuthProvider, signInWithPopup} from 'firebase/auth'
-import { app } from "../firebase";  
-import {useDispatch} from "react-redux" 
-import {signInSuccess} from '../redux/user/userSlice';
-import {useNavigate} from "react-router-dom"
+import React, { useState } from 'react'
+import { Link, useNavigate } from "react-router-dom"
+import OAuth from '../Components/OAuth.jsx'
 
-export default function OAuth() {
-  const dispatch=useDispatch();
-  const navigate=useNavigate();
-  const handleGoogleClick=async()=>{
+export default function SignUp() {
+  const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     try {
-      const provider=new GoogleAuthProvider();
-      const auth=getAuth(app);
-      const result=await signInWithPopup(auth,provider);
-     const res=await fetch("/api/auth/google",{
-      method:"POST",
-      headers:{
-        'Content-Type':'application/json',
-      },
-      body:JSON.stringify({name:result.user.displayName,email:result.user.email,photo:result.user.photoURL})
+      setLoading(true);
 
-     });
-     const data=await res.json();
-     dispatch(signInSuccess(data));
-     navigate("/");
+      const res = await fetch(
+        `${import.meta.env.VITE_BACKEND_API_URL}/auth/signup`,
+        {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json',
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      const data = await res.json();
+      console.log(data);
+
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
     } catch (error) {
-      console.log("could not sign with google",error);
+      setLoading(false);
+      setError(error.message);
     }
-  }
+  };
+
   return (
-      <button onClick={handleGoogleClick} type='button' className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95'>Continue with google</button>
-  
+    <div className='p-3 max-w-lg mx-auto'>
+      <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+      <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder='password'
+          className='border p-3 rounded-lg'
+          id='password'
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+        >
+          {loading ? 'Loading...' : 'Sign Up'}
+        </button>
+        <OAuth />
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Have an account?</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-700'>Sign in</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
+    </div>
   )
 }
