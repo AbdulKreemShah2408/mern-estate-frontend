f121a2f53bff2e904015ff5e2c78a2786709cede
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2026-04-14 21:33:03 -0700

    Modify Firebase config for new project
    Updated Firebase configuration with new project details.

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-estate-1babf.firebaseapp.com",
-  projectId: "mern-estate-1babf",
- storageBucket: "mern-estate-1babf.appspot.com",
-  messagingSenderId: "201968412903",
-  appId: "1:201968412903:web:34857bb46463afd4fc1a42",
-  measurementId: "G-94ZKHSHQ10"
+  authDomain: "mern-estate-5585a.firebaseapp.com",
+  projectId: "mern-estate-5585a",
+  storageBucket: "mern-estate-5585a.firebasestorage.app",
+  messagingSenderId: "930329421751",
+  appId: "1:930329421751:web:41d377ab9625a41a56aa32"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
