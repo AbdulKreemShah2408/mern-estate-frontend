3a69fedc3e71dabae87159f5686bcc7af04c1a1c
Abdul Kreem Shah <abdulkreemshah92@gmail.com>
2026-07-10 04:02:45 -0700

    Update estate name in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -54,7 +54,8 @@ export default function Home() {
           place with ease
         </h1>
         <div className="text-gray-400 text-xs sm:text-sm">
-          Abdul Kreem Shah Estate is the best place to find your next perfect place to live
+          Sahand
+Estate is the best place to find your next perfect place to live
           <br />
           We have a wide range of properties for you to choose from.
         </div>
